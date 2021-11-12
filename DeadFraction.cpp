4e7d9d5eec767acf5399bf51d841b69b9fc0d093
Connor Lukan <93485697+ConnorLukan87@users.noreply.github.com>
2021-11-11 21:16:30 -0600

    Update DeadFraction.cpp

--- DeadFraction.cpp
+++ DeadFraction.cpp
@@ -1,36 +1,47 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
+#include <cmath>
 
-unsigned int gcd(unsigned int n, unsigned int d) 
-{return ((n%d==0) ? d : gcd(d,n%d));}
 
-unsigned int pow10(int exponent)
+struct fraction
 {
-  unsigned int t = 1;
-  for (int i=0;i<exponent;i++)
-  {
-    t *= 10;
-  }
-  return t;
-}
+  long n,d;
+};
+
+long gcd(long n, long d) { return (n%d==0) ? d : gcd(d,n%d);}
 
 int main()
 {
-  std::string s; std::cin >> s;
-  while (s.length()!=1)
+  std::string digits;
+  std::cin >> digits;
+  while (digits.length()!=1)
   {
-    std::reverse(s.begin(),s.end());
-    unsigned int i=4, n = 0;
-    while (s[i]!='.')
+    digits = digits.substr(2,digits.length()-5);
+    int c = 1; 
+    std::string m;
+    fraction best;
+     best.n=0;
+     best.d = 999999999;
+    for (int j=digits.length()-1; j>=0; --j,++c) // digits[j] = n_j, c = m.length();
     {
-      n += pow10(i-4)*(s[i]-'0');
-      i++;
+      m = digits.substr(j,digits.length()); // repeating part
+      fraction current; current.d = pow(10,j)*(pow(10,c)-1); // taken care of 
+      current.n =0;
+      for (int i=j;i>0;i--)
+      {
+        current.n += (digits[i-1]-'0')*pow(10,j-i); // hard numerator part
+
+      }
+      current.n = ((current.n)*(pow(10,c)-1) + atol(m.c_str()));
+      long gcf = gcd(current.n,current.d);
+      current.d = current.d/gcf;
+      current.n = current.n/gcf;
+      if (current.d<best.d)
+      {
+        best = current;
+      }
     }
-    n = (n*9) +(s[3]-'0');
-    unsigned int d = 9*pow10(i-4);
-    unsigned int f = gcd(n,d);
-    std::cout << n/f << "/" << d/f << std::endl;
-    std::cin >> s;
+    std::cout << best.n << "/" << best.d << std::endl;
+    std::cin >> digits;
   }
 }
